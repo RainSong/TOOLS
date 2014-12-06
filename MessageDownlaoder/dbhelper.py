d8291d8775a88a6b75e9a66b174b2acf263bb144
RainSong <714224605@qq.com>
2014-12-06 16:49:38 +0800

    已经可以读取并且往数据库存数据了 Signed-off-by: RainSong <714224605@qq.com>

--- MessageDownlaoder/dbhelper.py
+++ MessageDownlaoder/dbhelper.py
@@ -1,12 +1,30 @@
+import os
+import time
 import sqlite3
 
-def read_some(sql):
-	print(sql)
-	
-def execute(sql):
-	print(sql)
-	
-def insert_new_page():
-	print('')
-	
+dbPath = os.getcwd() + '\\Data\\2014.db'
 
+def exists_url(md5):
+    conn = sqlite3.connect(dbPath,1000)
+    sql = "select count(1) from urls where md5 = '{0}'".format(md5)
+    cursor = conn.cursor()
+    print(sql)
+    cursor.execute(sql)
+    dr = cursor.fetchone()
+    conn.close()
+    return dr[0] > 0
+
+def add_url(urls):
+   conn = sqlite3.connect(dbPath,timeout=1000)
+   print('connnect db success')
+   sql = ''
+   for url in urls:
+       #addTime = time.time()
+       #addTime = time.localtime(addTime)
+       #addTime = time.strftime('%Y-%m-%d %H:%M:%S',addTime)
+       if exists_url(url[1]) == False:
+           sql = sql + "insert into urls(url,md5) values('{0}','{1}');\n".format(url[0],url[1])
+   print(sql)
+   conn.executescript(sql)
+   conn.commit()
+   conn.close()
--- MessageDownlaoder/reader.py
+++ MessageDownlaoder/reader.py
@@ -4,9 +4,17 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import urllib
+import dbhelper
+import hashlib
 
 head = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
 baseUrl = 'http://www.meizitu.com'
+
+def getmd5(str):
+    str = str.encode()
+    m = hashlib.md5()
+    m.update(str)
+    return m.hexdigest()
 
 def read_child_page(url):
     print(url)
@@ -16,12 +24,19 @@ def read_page(url):
     content = page.content
     contentSoup = BeautifulSoup(content,from_encoding='gb18030')
     picture_divs = contentSoup.find_all('div',{'class':'postContent'})
+    cheildUrls = []
     for picture_div in picture_divs:
         child_page_url = picture_div.find('a').attrs['href']
-        read_child_page(child_page_url)
-    nextUrl = baseUrl + contentSoup.find('a',text='下一页').attrs['href']
+        cheildUrls.append([child_page_url,getmd5(child_page_url)])
+        #read_child_page(child_page_url)
+    dbhelper.add_url(cheildUrls)
+    nextUrl = contentSoup.find('a',text = '下一页').attrs['href']
+    if nextUrl.startswith('/a'):
+        nextUrl = baseUrl + nextUrl
+    else:
+        nextUrl = baseUrl + '/a/' + nextUrl
     read_page(nextUrl)
     print(url)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     read_page(baseUrl)
