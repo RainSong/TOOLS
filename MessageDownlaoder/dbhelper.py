259ee06a83a55ebf5f4a99323e2708df35202055
RainSong <714224605@qq.com>
2014-12-12 13:48:36 +0800

    add some thing

--- MessageDownlaoder/dbhelper.py
+++ MessageDownlaoder/dbhelper.py
@@ -1,9 +1,16 @@
 import os
 import time
 import sqlite3
+import logging
 
 dbPath = os.getcwd() + '\\Data\\2014.db'
 
+<<<<<<< HEAD
+logPath = os.getcwd() + '\\Logs\\' + time.strftime('%Y-%m-%d',time.localtime(time.time())) + '.log'
+
+#logging.basicConfig(filename=logPath)
+=======
+>>>>>>> d8291d8775a88a6b75e9a66b174b2acf263bb144
 def exists_url(md5):
     conn = sqlite3.connect(dbPath,1000)
     sql = "select count(1) from urls where md5 = '{0}'".format(md5)
@@ -19,12 +26,78 @@ def add_url(urls):
    print('connnect db success')
    sql = ''
    for url in urls:
+<<<<<<< HEAD
+=======
        #addTime = time.time()
        #addTime = time.localtime(addTime)
        #addTime = time.strftime('%Y-%m-%d %H:%M:%S',addTime)
+>>>>>>> d8291d8775a88a6b75e9a66b174b2acf263bb144
        if exists_url(url[1]) == False:
            sql = sql + "insert into urls(url,md5) values('{0}','{1}');\n".format(url[0],url[1])
    print(sql)
    conn.executescript(sql)
    conn.commit()
    conn.close()
+<<<<<<< HEAD
+
+def get_top_urls(top_num):
+    sql = 'select  id,url from urls where is_readed in (0,1) order by add_time desc limit 0,{0}'.format(top_num)
+    conn = sqlite3.connect(dbPath,timeout=1000)
+    cur = conn.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def set_url_reading(ids):
+    sql = 'update urls set is_readed = 1 where id in ({0})'.format(ids)
+    conn = sqlite3.connect(dbPath,timeout=1000)
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
+    return cur.rowcount
+
+def add_tag(page_id,tags,cur):
+    exists_tag_sql = "select id from tags where tag = '{0}' limit 0,1"
+    insert_tag_sql = "insert into tags (tag) values('{0}')"
+    insert_page_tag_sql = "insert into page_tag(page_id,tag_id) values({0},{1})"
+    tag_id = 0
+    for tag in tags:
+        cur.execute(exists_tag_sql.format(tag))
+        if cur.rowcount < 0:
+           cur.execute(insert_tag_sql.format(tag))
+           cur.execute("select last_insert_rowid()")
+           row = cur.fetchone()
+           tag_id = row[0]
+        else:
+            row = cur.fetchone()
+            tag_id = row[0]
+        cur.execute(insert_page_tag_sql.format(page_id,tag_id))
+
+def add_page(url_id,encoding,content,title,description,tags):
+    exists_page_saq = "select id from page_info where url_id={0}".format(url_id)
+    sql = "insert into page_info (url_id,encoding,content,title,description) values ({0},'{1}','{2}','{3}','{4}')".format(url_id,encoding,content,title,description)
+    conn = sqlite3.connect(dbPath,timeout=1000)
+    cur = conn.cursor()
+    try:
+        cur.execute(sql)
+        row = cur.fetchone()
+        if cur.rowcount < 0:
+            cur.execute(sql)
+            cur.execute("select last_insert_rowid()")
+            row = cur.fetchone()
+            page_id = row[0]
+            add_tag(page_id,tags,cur)
+        else:
+            conn.close()
+            return  row[0]
+        conn.commit()
+    except sqlite3.Error as e:
+        print(e)
+        conn.rollback()
+    finally:
+        conn.close()
+    return page_id
+=======
+>>>>>>> d8291d8775a88a6b75e9a66b174b2acf263bb144
--- MessageDownlaoder/reader.py
+++ MessageDownlaoder/reader.py
@@ -16,8 +16,38 @@ def getmd5(str):
     m.update(str)
     return m.hexdigest()
 
+<<<<<<< HEAD
+def downImg(url,url_id):
+    page = requests.session().get(url,headers=head)
+    content = page.content
+    #encoding = content.encoding
+    contentSoup = BeautifulSoup(content,from_encoding='gb18030')
+    infobox = contentSoup.find('div',{'class':'boxinfo'})
+    infos = infobox.find_all('p')
+    title = infos[0].text
+    description = ''
+    if len(infos) >= 2:
+        description = infos[0].text + '\r\n' + infos[1].text
+    tagStr = contentSoup.find('div',{'class':'metaRight'}).find('p').text
+    pattern = re.compile(u'[\u4E00-\u9FFF]+')
+    tags = pattern.findall(tagStr)
+    
+    dbhelper.add_page(url_id,'','',title,description,tags)
+
+def read_child_page():
+    rows = dbhelper.get_top_urls(10)
+    ids = ''
+    for row in rows:
+        if(len(ids) > 0):
+            ids = ids + ','
+        ids = ids + str(row[0])
+    if  dbhelper.set_url_reading(ids) > 0:
+        for row in rows:
+            downImg(row[1],row[0])
+=======
 def read_child_page(url):
     print(url)
+>>>>>>> d8291d8775a88a6b75e9a66b174b2acf263bb144
 
 def read_page(url):
     page = requests.session().get(url,headers=head)
@@ -28,7 +58,11 @@ def read_page(url):
     for picture_div in picture_divs:
         child_page_url = picture_div.find('a').attrs['href']
         cheildUrls.append([child_page_url,getmd5(child_page_url)])
+<<<<<<< HEAD
+    
+=======
         #read_child_page(child_page_url)
+>>>>>>> d8291d8775a88a6b75e9a66b174b2acf263bb144
     dbhelper.add_url(cheildUrls)
     nextUrl = contentSoup.find('a',text = '下一页').attrs['href']
     if nextUrl.startswith('/a'):
@@ -37,6 +71,12 @@ def read_page(url):
         nextUrl = baseUrl + '/a/' + nextUrl
     read_page(nextUrl)
     print(url)
+   
 
 if __name__ == '__main__':
+<<<<<<< HEAD
+    #read_page(baseUrl)
+    read_child_page()
+=======
     read_page(baseUrl)
+>>>>>>> d8291d8775a88a6b75e9a66b174b2acf263bb144
