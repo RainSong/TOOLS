6f877ed56c9ea95dd6c881048ddabea68161d19f
RainSong <714224605@qq.com>
2015-02-06 16:26:48 +0800

    aaa

--- MyReader/dataHelper.py
+++ MyReader/dataHelper.py
@@ -1,3 +1,19 @@
 #coding:utf-8
 
-import sqlalchemy
+import os
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import sessionmaker
+
+dbPath = os.path.split(os.path.realpath(__file__))[0] + '\\Data\\meizitu2015.db'
+engine = create_engine(dbPath,echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def get_tags():
+    """获取所有的标签"""
+    pass
+
+def get_readed_page(pageIndex,pageSize):
+    """获取页面"""
+    pass
--- MyReader/models.py
+++ MyReader/models.py
@@ -1,99 +1,17 @@
 #coding:utf-8
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String,DateTime
 import datetime
 
 base = declarative_base()
-
-#class url_table(base):
-#    __tablename__ = "url"
-
-#    id = Column(Integer,primary_key=True)
-#    url = Column(String)
-#    add_time = Column(String)
-#    md5 = Column(String)
-#    is_readed = Column(Integer,default=0)
-
-#class tag_table(base):
-#    __tablename__ = "tag"
-
-#    id = Column(Integer,primary_key=True)
-#    tag = Column(String)
-#    add_time = Column(datetime,default=datetime.now)
-#    citations = Column(Integer,default=1)
-
-#class page_tag_table(base):
-#    __tablename__ = "page_tag"
-
-#    id = Column(Integer,primary_key=True)
-#    page_id = Column(Integer)
-#    tag_id = Column(Integer)
-#    add_time = Column(datetime,default=datetime.now)
-
-#class page_info_table(base):
-#    __tablename__ = "page_info"
-
-#    id = Column(Integer,primary_key=True)
-#    url_id = Column(Integer)
-#    encoding = Column(String)
-#    content = Column(string)
-#    title = Colunm(String)
-#    description = Column(String)
-#    add_time = Column(datetime,default=datetime.now)
-#    is_readed = Column(Integer,default=0)
-
-#class page_file_table(base):
-#    __tablename__ = "page_file"
-
-#    id = Column(Integer,primary_key=True)
-#    page_id = Column(Integer)
-#    file_id = Column(Integer)
-#    add_time = Column(datetime,default=datetime.now)
-
-#class file_info_table(base):
-#    __tablename__= "file_info"
-
-#    id = Column(Integer,primary_key=True)
-#    md5 = Column(String)
-#    extension = Column(String)
-#    content = Column(String)
-#    size = Column(Integer)
-#    url = Column(String)
-#    add_time = Column(datetime,default=datetime.now)
-
-#class url(self):
-#   pass
-
-#class tag(self):
-#    pass
-
-#class page_tag(self):
-#    pass
-
-#class page_info(self):
-#    pass
-
-#class page_file(self):
-#    pass
-
-#class file_info(self):
-#    pass
-
-#mapper(url,url_table)
-#mapper(tag,tag_talbe)
-#mapper(page_tag,page_tag_table)
-#mapper(page_info,page_info_table)
-#mapper(page_file,page_file_table)
-#mapper(file_info,file_info_table)
-
 
 class url(base):
     __tablename__ = "url"
 
     id = Column(Integer,primary_key=True)
     url = Column(String)
-    add_time = Column(String)
+    add_time = Column(DateTime,default=datetime.datetime.now())
     md5 = Column(String)
     is_readed = Column(Integer,default=0)
 
@@ -102,7 +20,7 @@ class tag(base):
 
     id = Column(Integer,primary_key=True)
     tag = Column(String)
-    add_time = Column(datetime,default=datetime.now)
+    add_time = Column(DateTime,default=datetime.datetime.now())
     citations = Column(Integer,default=1)
 
 class page_tag(base):
@@ -111,7 +29,7 @@ class page_tag(base):
     id = Column(Integer,primary_key=True)
     page_id = Column(Integer)
     tag_id = Column(Integer)
-    add_time = Column(datetime,default=datetime.now)
+    add_time = Column(DateTime,default=datetime.datetime.now())
 
 class page_info(base):
     __tablename__ = "page_info"
@@ -119,10 +37,10 @@ class page_info(base):
     id = Column(Integer,primary_key=True)
     url_id = Column(Integer)
     encoding = Column(String)
-    content = Column(string)
-    title = Colunm(String)
+    content = Column(String)
+    title = Column(String)
     description = Column(String)
-    add_time = Column(datetime,default=datetime.now)
+    add_time = Column(datetime,default=datetime.datetime.now())
     is_readed = Column(Integer,default=0)
 
 class page_file(base):
@@ -131,7 +49,7 @@ class page_file(base):
     id = Column(Integer,primary_key=True)
     page_id = Column(Integer)
     file_id = Column(Integer)
-    add_time = Column(datetime,default=datetime.now)
+    add_time = Column(datetime,default=datetime.datetime.now())
 
 class file_info(base):
     __tablename__= "file_info"
@@ -142,4 +60,4 @@ class file_info(base):
     content = Column(String)
     size = Column(Integer)
     url = Column(String)
-    add_time = Column(datetime,default=datetime.now)
+    add_time = Column(datetime,default=datetime.datetime.now())
